4d45c1af5b06f89733b01f57a0abff095a3bb4c7
Bloo00 <kabin.yangg@gmail.com>
2021-11-16 16:38:42 -0800

    lesson done

--- index.js
+++ index.js
@@ -20,3 +20,51 @@ console.log(myModule);
 myModule.hello(); // string
 
 console.log(myModule.hello());
+
+// to make a local server
+
+const http = require('http');
+
+// http.createServer((req, res) => { 
+//    res.write('Hello, World!');
+//    res.end();
+// })
+// .listen(8000);
+
+// read story .txt file
+
+const fs = require('fs');
+
+fs.readFile('story.txt', 'utf8', function(err, data){
+    if(err) {
+        console.log("There was a problem reading the file.");
+    } else {
+        console.log(data);
+    }
+});
+
+// import
+const faker = require('faker'); // random stuff
+let randomName = faker.name.findName();
+console.log(randomName);
+
+const randomeAddress = {
+    streetAddress: faker.address.streetAddress(),
+    city: faker.address.city(),
+    zipcode: faker.address.zipCode()
+}
+
+console.log(randomName);
+
+// import the module
+const axios = require('axios'); // allows to make API calls and retrieve data...
+
+// There is a function that exist on the frontend that allows us to retrieve data. What function? - fetch
+
+axios.get('https://api.github.com/users/Bloo00')
+.then(function(response) {
+    console.log(response.data);
+})
+.catch(function(err) {
+    console.log(err);
+});
